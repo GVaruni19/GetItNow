d113cc827f10adc00d7fd63dfdd9b42f754ff355
GVaruni19 <61648707+GVaruni19@users.noreply.github.com>
2021-03-18 12:16:06 +0530

    updated commit

--- GetItNow/settings.py
+++ GetItNow/settings.py
@@ -30,12 +30,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a=m+1^c$evt6l5$f=(9ea^7n7r#r8rpjdu57&(((8ms(gvx2g('
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [getitnowapp.herokuapp.com]
 
 
 # Application definition
